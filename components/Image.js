569f5a3c04512adb8e08e5e3c8cc6879f5b18351
Wim Put <wim.put@vrt.be>
2021-08-20 04:01:05 +0200

    test

--- components/Image.js
+++ components/Image.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
 export const Image = (Elm) => (props) => {
-    return <Elm style={{ width: 100, borderWidth: 5, borderStyle: 'solid', borderColor: '#000' }} {...props} />;
+    const {style, ...otherProps} = props;
+    return <Elm style={{ width: 100, height: 100, borderRadius: 50, ...style}} {...otherProps} />;
 }
