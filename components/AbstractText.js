60ae58b350091ee08e48a76b6eb54a0eeb1d6cf1
Wim Put <wim.put@vrt.be>
2021-08-20 02:24:39 +0200

    jsx to js

--- components/AbstractText.js
+++ components/AbstractText.js
@@ -3,5 +3,5 @@ import React from 'react';
 const TOKENS = { color: '#F00' };
 
 export const AbstractText = (elm) => (props) => {
-    return <elm style={{ color: TOKENS.color }} {...props} />;
+    return <elm style={{ color: TOKENS.color, fontSize: 24 }} {...props} />;
 }
